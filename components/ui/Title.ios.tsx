e2b4f83808bb532858d31824923ec31e8dbfd180
Taerande <revive0405@gmail.com>
2025-03-02 21:29:10 +0900

    feat : platform specific components

---
+++ components/ui/Title.ios.tsx
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet, Platform } from "react-native";
+import React from "react";
+import Colors from "../../constants/colors";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Title: React.FC<Props> = ({ children, ...rest }) => {
+  return <Text style={styles.title}>{children}</Text>;
+};
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "open-sans-bold",
+    marginTop: 30,
+    padding: 12,
+    fontSize: 24,
+    color: "white",
+    textAlign: "center",
+    borderWidth: 0,
+    borderColor: "white",
+    maxWidth: "80%",
+  },
+});
