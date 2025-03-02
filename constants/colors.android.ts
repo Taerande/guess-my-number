89e2f8de0a36a8c5a9dfed27204868451e472575
Taerande <revive0405@gmail.com>
2025-03-02 21:44:03 +0900

    style : 플랫폼 별 색상

---
+++ constants/colors.android.ts
@@ -0,0 +1,16 @@
+const Colors = {
+  // colors with blue shades
+  primary100: "#b3e5fc",
+  primary200: "#81d4fa",
+  primary300: "#4fc3f7",
+  primary400: "#29b6f6",
+  primary500: "#03a9f4",
+  primary600: "#039be5",
+  primary700: "#0288d1",
+  primary800: "#0277bd",
+  primary900: "#01579b",
+
+  accent500: "#ddb52f",
+};
+
+export default Colors;
